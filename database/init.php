8595061a76bb6fbcaa1b6759c28fcbc494955c12
QuentinLequeux <145542949+QuentinLequeux@users.noreply.github.com>
2024-02-24 17:04:41 +0100

    Add changes

--- database/init.php
+++ database/init.php
@@ -3,8 +3,17 @@
 const BASE_PATH = __DIR__ . '/..';
 require BASE_PATH . '/vendor/autoload.php';
 
-require __DIR__.'/migration.php';
+require __DIR__ . '/migration.php';
 
 if ($argc === 2 && $argv[1] === 'seed') {
     require __DIR__ . '/seed.php';
 }
+
+/*
+
+$argv
+$argc
+composer init
+composer dump autoload
+
+*/
